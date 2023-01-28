6c8f58bdcbd56172da1fa3a8eb8ba091fcd782cf
Danail <d_krasimirov@yahoo.com>
2023-01-28 22:42:37 +0100

    new recipe post

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dkr290/go-recipes/handlers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
+	handler := handlers.NewHandlers()
+
 	router := gin.Default()
+	router.POST("/recipes", handler.NewRecipeHandler)
 	router.Run("127.0.0.1:8080")
 
 }
