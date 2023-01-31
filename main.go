0d14ea0265e1ed44b29e3abe10bfcab3ba4ee0fc
Danail Surudzhiyski <dans@bankingcircle.com>
2023-01-31 18:28:51 +0100

    mongodbadding

--- main.go
+++ main.go
@@ -20,7 +20,30 @@ package main
 import (
 	"github.com/dkr290/go-recipes/handlers"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var err error
+var client *mongo.Client
+
+func init() {
+
+	// recipes := models.GetAll()
+	// file, _ := os.ReadFile("recipes.json")
+	// _ = json.Unmarshal([]byte(file), &recipes)
+	// var listOfRecipes []interface{}
+	// for _, recipe := range recipes {
+	// 	listOfRecipes = append(listOfRecipes, recipe)
+	// }
+	// collection := client.Database(db).Collection("recipes")
+	// log.Println(collection)
+	// inseartManyResult, err := collection.InsertMany(context.Background(), listOfRecipes)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println("inserted recipes:", len(inseartManyResult.InsertedIDs))
+
+}
 
 func main() {
 
--- models/model.go
+++ models/model.go
@@ -1,7 +1,17 @@
 package models
 
 import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // swagger:parameters recipes get all
@@ -14,6 +24,10 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var mclient *mongo.Client
+var db string
+var ctx = context.Background()
+
 func GetAll() []Recipe {
 
 	rr := make([]Recipe, 0)
@@ -24,4 +38,77 @@ func GetRecipe() Recipe {
 
 	var r1 Recipe
 	return r1
+}
+
+func Connect() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	db = os.Getenv("MONGO_DATABASE")
+	if db == "" {
+		log.Fatal("You must set your 'MONGODB_DATABASE' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	createdb := os.Getenv("INITIAL_CREATEDB")
+	if createdb == "" {
+		log.Fatal("You must set your 'INITIAL_CREATEDB' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	mclient, _ = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err := mclient.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to Mongo DB server")
+	defer func() {
+		if err := mclient.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	if createdb == "true" {
+		initialDbCreate()
+
+	}
+
+}
+
+func initialDbCreate() {
+
+	recipes := make([]Recipe, 0)
+	file, _ := os.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+	var listOfRecipes []interface{}
+	for _, recipe := range recipes {
+		listOfRecipes = append(listOfRecipes, recipe)
+	}
+	collection := mclient.Database(db).Collection("recipes")
+	log.Println(collection)
+	inseartManyResult, err := collection.InsertMany(context.Background(), listOfRecipes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("inserted recipes:", len(inseartManyResult.InsertedIDs))
+}
+
+func ListRecipes() ([]Recipe, error) {
+
+	collection := mclient.Database(db).Collection("recipes")
+	curr, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer curr.Close(ctx)
+	recipes := make([]Recipe, 0)
+	for curr.Next(ctx) {
+		var recipe Recipe
+		curr.Decode(&recipe)
+		recipes = append(recipes, recipe)
+
+	}
+	log.Println(recipes)
+	return recipes, nil
+
 }
